43d1bc4c5cdfaa6aaa44ffbb97cac1c6f9a40190
Jagdish <2021ucp1385@mnit.ac.in>
2024-06-16 02:06:05 +0530

    authentication

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const UserController = require('../controllers/user_controller');
+const UserController = require('../controllers/users_controller');
 
 router.get('/profile', UserController.profile);
 
