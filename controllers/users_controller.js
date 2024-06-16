35cdd4d65407af95461d29d24d98d46f17ad86b4
Jagdish <2021ucp1385@mnit.ac.in>
2024-06-17 02:36:59 +0530

    Passing User data to views and restricting page access

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,19 +6,29 @@ module.exports.profile = function(req, res) {
     });
 };
 
-// Render the sign up page
-module.exports.signUp = function(req, res) {
+// render the sign up page
+module.exports.signUp = function(req, res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
-    });
-};
+    })
+}
 
-// Render the sign in page
-module.exports.signIn = function(req, res) {
+
+// render the sign in page
+module.exports.signIn = function(req, res){
+
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
-    });
-};
+    })
+}
 
 // Get the sign up data
 module.exports.create = async function(req, res) {
